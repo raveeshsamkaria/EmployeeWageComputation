77044e9c1dc2ca4b23f905189e0077e9a8324e6d
Raveesh Samkaria <rishu9417@gmail.com>
2020-09-25 17:53:21 +0530

    Added : Store the Wage of Companies

--- employeeWageComputation.java
+++ employeeWageComputation.java
@@ -1,15 +1,41 @@
 public class employeeWageComputation
 {
-    static final int PRESENT=1;
+    public static int wageComputation(String companyName, int empRatePerHr, int noOfWorkDay, int maxHrsInMonth)
+    {
+        final int is_Part_Time=2, is_Full_Time=1;
+        int empHrs=0, totalEmpHrs = 0, totalWorkDay=0, totalEmpWage=0;
+
+        System.out.println(">---< Welcome To " + companyName+ " >---<");
+        while( totalWorkDay < noOfWorkDay && totalEmpHrs <= maxHrsInMonth )
+        {
+            int employee=(int)(Math.random()*10)%3;
+            switch(employee)
+            {
+                case is_Part_Time:	empHrs = 4;
+                                    break;
+                case is_Full_Time:	empHrs = 8;
+                                    break;
+                default:			empHrs = 0;
+            }
+            totalWorkDay++;
+            totalEmpHrs += empHrs;
+        }
+        totalEmpWage = totalEmpHrs * empRatePerHr;
+        return totalEmpWage;
+    }
+
     public static void main(String[] args)
     {
         System.out.println(">---< EMPLOYEE WAGE COMPUTATION >---<");
         System.out.println("-------------------------------------");
-        int employee=(int)(Math.random()*10)%2;
-        if(PRESENT==employee)
-            System.out.println("Employee Is Present");
-        else
-            System.out.println("Employee Is Absent");
+        employeeWageComputation emp = new employeeWageComputation();
+        int []wages=new int[3];
+        wages[0]=emp.wageComputation("HP", 20, 20, 100);
+        System.out.println("Employee Monthly Wage Is "+wages[0] + " In HP");
+        wages[1]=emp.wageComputation("DELL", 30, 25, 120);
+        System.out.println("Employee Monthly Wage Is "+wages[1] + " In DELL");
+        wages[2]=emp.wageComputation("ACER", 40, 20, 150);
+        System.out.println("Employee Monthly Wage Is "+wages[2] + " In ACER");
     }
 }
 
