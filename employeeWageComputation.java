bb5d1925f4def953f8a9f757e8694377a6b77d5c
Raveesh Samkaria <rishu9417@gmail.com>
2020-09-25 15:18:17 +0530

    Added : Employee Attendance functionality

--- employeeWageComputation.java
+++ employeeWageComputation.java
@@ -1,1 +1,15 @@
+public class employeeWageComputation
+{
+    static final int PRESENT=1;
+    public static void main(String[] args)
+    {
+        System.out.println(">---< EMPLOYEE WAGE COMPUTATION >---<");
+        System.out.println("-------------------------------------");
+        int employee=(int)(Math.random()*10)%2;
+        if(PRESENT==employee)
+            System.out.println("Employee Is Present");
+        else
+            System.out.println("Employee Is Absent");
+    }
+}
 
