c45b50acd99285711fdeaa45daecb3bb0546119e
Raveesh Samkaria <rishu9417@gmail.com>
2020-09-25 15:54:16 +0530

    Added : Switch Case Statement functionality

--- employeeWageComputation.java
+++ employeeWageComputation.java
@@ -1,15 +1,27 @@
 public class employeeWageComputation
 {
-    static final int PRESENT=1;
     public static void main(String[] args)
     {
         System.out.println(">---< EMPLOYEE WAGE COMPUTATION >---<");
         System.out.println("-------------------------------------");
-        int employee=(int)(Math.random()*10)%2;
-        if(PRESENT==employee)
-            System.out.println("Employee Is Present");
-        else
-            System.out.println("Employee Is Absent");
+
+        final int empRatePerHr=20, is_Part_Time=2, is_Full_Time=1;
+        int empWage=0, empHrs=0;
+
+        int employee=(int)(Math.random()*10)%3;
+        switch(employee)
+        {
+            case is_Part_Time:	System.out.println("Employee Is Part Time Present");
+                empHrs = 4;
+                break;
+            case is_Full_Time:	System.out.println("Employee Is Full Time Present");
+                empHrs = 8;
+                break;
+            default:			System.out.println("Employee Is Absent ");
+                empHrs = 0;
+        }
+        empWage = empHrs * empRatePerHr;
+        System.out.println("Employee Wage : "+empWage);
     }
 }
 
