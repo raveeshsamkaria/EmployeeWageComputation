c71bcff4b3c2813128bb3da426d5136eaed62e43
Raveesh Samkaria <rishu9417@gmail.com>
2020-09-25 15:46:44 +0530

    Added : Employee Part Time Wages functionality

--- employeeWageComputation.java
+++ employeeWageComputation.java
@@ -1,15 +1,31 @@
 public class employeeWageComputation
 {
-    static final int PRESENT=1;
     public static void main(String[] args)
     {
         System.out.println(">---< EMPLOYEE WAGE COMPUTATION >---<");
         System.out.println("-------------------------------------");
-        int employee=(int)(Math.random()*10)%2;
-        if(PRESENT==employee)
-            System.out.println("Employee Is Present");
+
+        int PRESENT=1, empRatePerHr=20, is_Part_Time=2, is_Full_Time=1;
+        int empWage=0, empHrs=0;
+
+        int employee=(int)(Math.random()*10)%3;
+        if(employee == is_Part_Time)
+        {
+            System.out.println("Employee Is Part Time Present");
+            empHrs=4;
+        }
+        else if (employee == is_Full_Time )
+        {
+            System.out.println("Employee Is Full Time Present");
+            empHrs=8;
+        }
         else
+        {
             System.out.println("Employee Is Absent");
+            empHrs=0;
+        }
+        empWage = empHrs * empRatePerHr;
+        System.out.println("Employee Wage : "+empWage);
     }
 }
 
